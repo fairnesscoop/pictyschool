e6fddfb507b1598cdb7e6d8b426dea291d96ff10
volubyl <volubyl@pm.me>
2020-09-25 15:22:10 +0200

    Update .gitignorefile

--- api/dist/Domain/Photographer/Photographer.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Quote {
-    private id;
-    private firstName;
-    private lastName;
-    private email;
-    private password;
-    constructor(email: string, firstname: string, password: string, lastname: string);
-}
