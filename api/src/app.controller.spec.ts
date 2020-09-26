81d8d6c3c1d066edfa7fd77ce9cf041eb13116c7
volubyl <volubyl@pm.me>
2020-09-26 14:49:45 +0200

    Improve eslint integration and format code according the eslint/prettier rules

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -1,6 +1,6 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +8,7 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService]
     }).compile();
 
     appController = app.get<AppController>(AppController);
