51f2114cc323ff7086decb0f4a2b4cc4855317b4
BertrandD <volubyl@pm.me>
2020-09-27 12:33:15 +0200

    Enable bracket spacing and trailling comma

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,7 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {TypeOrmModule} from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forRoot()],
