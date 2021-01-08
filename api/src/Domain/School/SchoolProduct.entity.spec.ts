5160cb77748c0d3d9590e55c13f8d5191a4619df
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2021-01-08 14:15:05 +0100

    School product model (#44)

---
+++ api/src/Domain/School/SchoolProduct.entity.spec.ts
@@ -0,0 +1,20 @@
+import { mock, instance } from 'ts-mockito';
+import { SchoolProduct } from './SchoolProduct.entity';
+import { School } from './School.entity';
+import { Product } from '../Product/Product.entity';
+
+describe('SchoolProductProduct', () => {
+  it('testGetters', () => {
+    const school = mock(School);
+    const product = mock(Product);
+    const schoolproduct = new SchoolProduct(
+      999,
+      instance(school),
+      instance(product)
+    );
+    expect(schoolproduct.getId()).toBeUndefined();
+    expect(schoolproduct.getUnitPrice()).toBe(999);
+    expect(schoolproduct.getProduct()).toBe(instance(product));
+    expect(schoolproduct.getSchool()).toBe(instance(school));
+  });
+});
