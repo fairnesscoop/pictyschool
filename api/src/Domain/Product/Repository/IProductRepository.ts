770627a6c157308510bb6cd420fb22c5212cb089
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2021-01-08 14:39:27 +0100

    Add SchoolProduct repository (#45)

--- api/src/Domain/Product/Repository/IProductRepository.ts
+++ api/src/Domain/Product/Repository/IProductRepository.ts
@@ -1,7 +1,7 @@
 import { Product } from '../Product.entity';
 
 export interface IProductRepository {
-  save(task: Product): Promise<Product>;
+  save(product: Product): Promise<Product>;
   findOneByTitle(title: string): Promise<Product | undefined>;
   findOneById(id: string): Promise<Product | undefined>;
   findProducts(page: number): Promise<[Product[], number]>;
